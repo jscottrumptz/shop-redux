5336b8fa5a5080622be9d76b761531261a08e565
J Scott Rumptz <jscott@rumptz.tech>
2021-04-21 09:15:24 -0500

    removed globalState.js and replaced with store.js. modified reducers.js to hold initital state for redux and also modified the export

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -12,7 +12,16 @@ import {
     TOGGLE_CART
   } from './actions';
 
-export const reducer = (state, action) => {
+ //  Define the initial state value for the app
+ const initialState = {
+    products: [],
+	cart: [],
+	cartOpen: false,
+	categories: [],
+	currentCategory: ''
+ }
+
+export const reducer = (state = initialState, action) => {
     switch (action.type) {
         // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with
         // an updated products array
@@ -97,6 +106,4 @@ export const reducer = (state, action) => {
     }
 };
 
-export function useProductReducer(initialState) {
-    return useReducer(reducer, initialState);
-  }
+export default reducer;
